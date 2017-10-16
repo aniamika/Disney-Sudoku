ff304bd5b8d8739504cdbb06b827c387d79ea78c
aniamika <aniamika90@gmail.com>
2017-10-16 14:42:50 +0200

    first picture

--- sudoku.js
+++ sudoku.js
@@ -1,15 +1,17 @@
 
 var boxes = document.querySelectorAll('.box');
-var scores = 0;
-var bigScores = 0;
 var boxesLeftTopContainer = document.querySelectorAll('.left-top-container .box');
 var boxesRightTopContainer = document.querySelectorAll('.right-top-container .box');
 var boxesLeftBottomContainer = document.querySelectorAll('.left-bottom-container .box');
 var boxesRightBottomContainer = document.querySelectorAll('.right-bottom-container .box');
+var scoresLeftTop = 0;
+var scoresRightTop = 0;
+var scoresLeftBottom = 0;
+var scoresRightBottom = 0;
 
 
 function colorize(box) {
-        this.innerText = '';
+  this.innerText = '';
   if ( box.dataset.color == "blue" ) {
     box.style.background = "rgb(24, 177, 237)";
   } else if ( box.dataset.color == "lightgray" ) {
@@ -24,285 +26,128 @@ function colorize(box) {
     box.style.background = "rgb(3, 173, 85)";
   } else if ( box.dataset.color == "violet" ) {
     box.style.background = "rgb(104, 48, 157)";
+  } else if ( box.dataset.color == "yellow" ) {
+    box.style.background = "rgb(252, 247, 196)";
   } else {
-    box.style.background = "rgb(255,253,177)";
+    box.style.background = "rgb(210, 253, 255)";
+  }
+}
+
+// reset margins when finished all 4 sudoku
+function resetMargins() {
+  // if (scoresLeftTop == 3 && scoresRightTop == 3) {
+  //   document.querySelector('.left-top-container').style.marginRight = "0";
+  //   document.querySelector('.right-top-container').style.marginLeft = "0";
+  // } else if (scoresLeftTop == 3 && scoresLeftBottom == 3) {
+  //   document.querySelector('.left-top-container').style.marginBottom = "0";
+  //   document.querySelector('.left-bottom-container').style.marginTop = "0";
+  // } else if (scoresRightTop == 3 && scoresRightBottom == 3) {
+  //   document.querySelector('.right-top-container').style.marginBottom = "0";
+  //   document.querySelector('.right-bottom-container').style.marginTop = "0";
+  // } else if (scoresLeftBottom == 3 && scoresRightBottom == 3) {
+  //   document.querySelector('.left-bottom-container').style.marginRight = "0";
+  //   document.querySelector('.right-bottom-container').style.marginLeft = "0";
+  // }
+  //
+  // if (scoresLeftTop == 3 && scoresRightTop == 3 && scoresLeftBottom == 3) {
+  //   document.querySelector('.left-top-container').style.margin = "0";
+  //   document.querySelector('.right-top-container').style.margin = "0";
+  //   document.querySelector('.left-bottom-container').style.margin = "0";
+  // }
+  if (scoresLeftTop == 45 && scoresRightTop == 45 && scoresLeftBottom == 45 && scoresRightBottom == 45) {
+    document.querySelector('.left-top-container').style.margin = "0";
+    document.querySelector('.right-top-container').style.margin = "0";
+    document.querySelector('.left-bottom-container').style.margin = "0";
+    document.querySelector('.right-bottom-container').style.margin = "0";
+  }
+}
+
+// reset innerText when finished one sudoku
+function resetInnerText() {
+  if (scoresLeftTop == 45) {
+    for(var i=0; i<boxesLeftTopContainer.length; i++) {
+      boxesLeftTopContainer[i].innerText = '';
+    }
+  }
+  if (scoresRightTop == 45) {
+    for(var i=0; i<boxesRightTopContainer.length; i++) {
+      boxesRightTopContainer[i].innerText = '';
+    }
+  }
+  if (scoresLeftBottom == 45) {
+    for(var i=0; i<boxesLeftBottomContainer.length; i++) {
+      boxesLeftBottomContainer[i].innerText = '';
+    }
+  }
+  if (scoresRightBottom == 45) {
+    for(var i=0; i<boxesRightBottomContainer.length; i++) {
+      boxesRightBottomContainer[i].innerText = '';
+    }
+  }
+}
+
+// reset borders when finished all 4 sudoku
+function resetBorders() {
+  if (scoresLeftTop == 45) {
+    for (var i=0; i<boxesLeftTopContainer.length; i++) {
+      boxesLeftTopContainer[i].classList.remove('border-bottom');
+      boxesLeftTopContainer[i].classList.remove('border-right');
+    }
+  }
+  if (scoresRightTop == 45) {
+    for (var i=0; i<boxesRightTopContainer.length; i++) {
+      boxesRightTopContainer[i].classList.remove('border-bottom');
+      boxesRightTopContainer[i].classList.remove('border-right');
+    }
+  }
+  if (scoresLeftBottom == 45) {
+    for (var i=0; i<boxesLeftBottomContainer.length; i++) {
+      boxesLeftBottomContainer[i].classList.remove('border-bottom');
+      boxesLeftBottomContainer[i].classList.remove('border-right');
+    }
+  }
+  if (scoresRightBottom == 45) {
+    for (var i=0; i<boxesRightBottomContainer.length; i++) {
+      boxesRightBottomContainer[i].classList.remove('border-bottom');
+      boxesRightBottomContainer[i].classList.remove('border-right');
+    }
   }
 }
 
 
-for ( var i = 0; i < boxes.length; i++ ) {
-  boxes[i].addEventListener("keypress", function(event) {
-    const keyName = event.key;
 
-    if ( keyName == this.dataset.number ) {
+  for ( var i = 0; i < boxes.length; i++ ) {
+    boxes[i].addEventListener("keypress", function(event) {
+      const keyName = event.key;
 
-      // // give the score when choose correct - only once
-      // if(!scores){
-     // 		 scores = scores + 1;
-      // }
-      scores = scores + 1;
-      console.log("scores - " + scores)
-      this.innerText = this.dataset.number;
-      colorize(this);
-      this.setAttribute("contenteditable", false);
-    } else {
-      // // block possibility to tap the wrong answer
-      // event.preventDefault();
-      // return false;
-      this.style.background = "rgb(255,255,255)";
-      this.innerText = '';
-    }
-  });
-}
-
-  // left top container counter
-  for ( var i=0; i < boxesLeftTopContainer.length; i++ ) {
-      // if one from 4 sudoku is correct
-
-      if ( scores == 5 ) {
-        bigScores += 1
-        console.log( bigScores + ' top left sudoku is correct' );
-        // boxesLeftTopContainer[i].classList.remove("border-right");
-        // console.log(boxesLeftTopContainer.classList)
-        // reset scores
-        scores = 0;
+      if ( keyName == this.dataset.number ) {
+        this.innerText = this.dataset.number;
+        colorize(this);
+        this.setAttribute("contenteditable", false);
+        // count scores
+        if (this.dataset.position == "leftTop") {
+          scoresLeftTop = scoresLeftTop + 1;
+          console.log("scoresLeftTop - " + scoresLeftTop)
+        } else if (this.dataset.position == "rightTop") {
+          scoresRightTop = scoresRightTop + 1;
+          console.log("scoresRightTop - " + scoresRightTop)
+        } else if (this.dataset.position == "leftBottom") {
+          scoresLeftBottom = scoresLeftBottom + 1;
+          console.log("scoresLeftBottom - " + scoresLeftBottom)
+        } else if (this.dataset.position == "rightBottom") {
+          scoresRightBottom = scoresRightBottom + 1;
+          console.log("scoresRightBottom - " + scoresRightBottom)
+        }
+        resetInnerText();
+        resetMargins();
+        resetBorders();
+      } else {
+        // // block possibility to tap the wrong answer
+        // event.preventDefault();
+        // return false;
+        // this.style.background = "rgb(255,255,255)";
+        this.innerText = '';
       }
+
+    });
   }
-
-
-    // right top container counter
-    for ( var i=0; i < boxesLeftTopContainer.length; i++ ) {
-        // if one from 4 sudoku is correct
-        if ( scores == 45 ) {
-          bigScores += 1
-          console.log( bigScores + ' top right sudoku is correct' );
-          // reset scores
-          scores = 0;
-        }
-    }
-
-    // left bottom container counter
-    for ( var i=0; i < boxesLeftBottomContainer.length; i++ ) {
-        // if one from 4 sudoku is correct
-        if ( scores == 45 ) {
-          bigScores += 1
-          console.log( bigScores + ' bottom left sudoku is correct' );
-          // reset scores
-          scores = 0;
-        }
-    }
-
-    // right bottom container counter
-    for ( var i=0; i < boxesRightBottomContainer.length; i++ ) {
-        // if one from 4 sudoku is correct
-        if ( scores == 45 ) {
-          bigScores += 1
-          console.log( bigScores + ' bottom right sudoku is correct' );
-          // reset scores
-          scores = 0;
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-  // boxes[i].addEventListener("click", function() {
-  //   this.style.color = 'gray';
-  // });
-  // boxes[i].addEventListener("blur", function() {
-  //   this.style.color = 'black';
-  // });
-// }
-
-
-
-// var redDivs = document.querySelectorAll('.red[contenteditable]');
-// var creamyDivs = document.querySelectorAll('.creamy[contenteditable]');
-// var darkgrayDivs = document.querySelectorAll('.darkgray[contenteditable]');
-// var greenDivs = document.querySelectorAll('.green[contenteditable]');
-// var violetDivs = document.querySelectorAll('.violet[contenteditable]');
-// var blueDivs = document.querySelectorAll('.blue[contenteditable]');
-// var lighgrayDivs = document.querySelectorAll('.lightgray[contenteditable]');
-//
-// // change color of innerText when the box is clicked
-// for ( var i = 0; i < boxes.length; i++ ) {
-//   boxes[i].addEventListener("click", function() {
-//     this.style.color = 'gray';
-//   });
-//   boxes[i].addEventListener("blur", function() {
-//     this.style.color = 'black';
-//   });
-// }
-//
-// // RED //
-// for ( var i = 0; i < redDivs.length; i++ ) {
-//   redDivs[i].addEventListener("keyup", function() {
-//     if ( this.innerHTML == 0 ) {
-//       this.style.background = "rgb(250, 3, 12)";
-//       this.innerText = '';
-//     }
-//   })
-// }
-//
-// for ( var i = 0; i < redDivs.length; i++ ) {
-//   redDivs[i].addEventListener("keypress", function(event) {
-//     const keyName = event.key;
-//     if ( keyName == 0 ) {
-//       this.innerText = '';
-//     } else {
-//       // block possibility to tap the wrong answer
-//       event.preventDefault();
-//       return false;
-//     }
-//   });
-// }
-//
-//
-// // CREAMY //
-// for ( var i = 0; i < creamyDivs.length; i++ ) {
-//   creamyDivs[i].addEventListener("keyup", function() {
-//     if ( this.innerText == 1 ) {
-//       this.style.background = "rgb(252, 198, 151)";
-//       this.innerText = '';
-//     }
-//   })
-// }
-//
-// for ( var i = 0; i < creamyDivs.length; i++ ) {
-//   creamyDivs[i].addEventListener("keypress", function(event) {
-//     const keyName = event.key;
-//     if ( keyName == 1 ) {
-//       this.innerText = '';
-//     } else {
-//       // block possibility to tap the wrong answer
-//       event.preventDefault();
-//       return false;
-//     }
-//   });
-// }
-//
-//
-// // DARKGRAY //
-// for ( var i = 0; i < darkgrayDivs.length; i++ ) {
-//   darkgrayDivs[i].addEventListener("keyup", function() {
-//     if ( this.innerText == 2 ) {
-//       this.style.background = "rgb(46, 42, 41)";
-//       this.innerText = '';
-//     }
-//   })
-// }
-//
-// for ( var i = 0; i < darkgrayDivs.length; i++ ) {
-//   darkgrayDivs[i].addEventListener("keypress", function(event) {
-//     const keyName = event.key;
-//     if ( keyName == 2 ) {
-//       this.innerText = '';
-//     } else {
-//       // block possibility to tap the wrong answer
-//       event.preventDefault();
-//       return false;
-//     }
-//   });
-// }
-//
-//
-// // GREEN //
-// for ( var i = 0; i < greenDivs.length; i++ ) {
-//     greenDivs[i].addEventListener("keyup", function() {
-//         if ( this.innerText == 3 ) {
-//             this.style.background = "rgb(3, 173, 85)";
-//             this.innerText = '';
-//         }
-//     })
-// }
-//
-// for ( var i = 0; i < greenDivs.length; i++ ) {
-//   greenDivs[i].addEventListener("keypress", function(event) {
-//     const keyName = event.key;
-//     if ( keyName == 3 ) {
-//       this.innerText = '';
-//     } else {
-//       // block possibility to tap the wrong answer
-//       event.preventDefault();
-//       return false;
-//     }
-//   });
-// }
-//
-//
-// // VIOLET //
-// for ( var i = 0; i < violetDivs.length; i++ ) {
-//   violetDivs[i].addEventListener("keyup", function() {
-//     if ( this.innerText == 4 ) {
-//       this.style.background = "rgb(104, 48, 157)";
-//       this.innerText = '';
-//     }
-//   })
-// }
-//
-// for ( var i = 0; i < violetDivs.length; i++ ) {
-//   violetDivs[i].addEventListener("keypress", function(event) {
-//     const keyName = event.key;
-//     if ( keyName == 4 ) {
-//       this.innerText = '';
-//     } else {
-//       // block possibility to tap the wrong answer
-//       event.preventDefault();
-//       return false;
-//     }
-//   });
-// }
-//
-//
-// // BLUE //
-// for ( var i = 0; i < blueDivs.length; i++ ) {
-//   blueDivs[i].addEventListener("keyup", function(event) {
-//     if ( this.innerText == 5 ) {
-//         this.style.background = "rgb(24, 177, 237)";
-//         this.innerText = '';
-//     }
-//   });
-// }
-//
-// for ( var i = 0; i < blueDivs.length; i++ ) {
-//   blueDivs[i].addEventListener("keypress", function(event) {
-//     const keyName = event.key;
-//     if ( keyName == 5 ) {
-//       this.innerText = '';
-//     } else {
-//       // block possibility to tap the wrong answer
-//       event.preventDefault();
-//       return false;
-//     }
-//   });
-// }
-//
-//
-// // LIGHTGRAY //
-// for ( var i = 0; i < lighgrayDivs.length; i++ ) {
-//   lighgrayDivs[i].addEventListener("keyup", function() {
-//     if ( this.innerText == 6 ) {
-//       this.style.background = "rgb(162, 165, 170)";
-//       this.innerText = '';
-//     }
-//   })
-// }
-//
-// for ( var i = 0; i < lighgrayDivs.length; i++ ) {
-//   lighgrayDivs[i].addEventListener("keypress", function(event) {
-//     const keyName = event.key;
-//     if ( keyName == 6 ) {
-//       this.innerText = '';
-//     } else {
-//       // block possibility to tap the wrong answer
-//       event.preventDefault();
-//       return false;
-//     }
-//   });
-// }
