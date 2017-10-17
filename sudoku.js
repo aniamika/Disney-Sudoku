173a01bac53127cf4b9fe8ed428f953fa2c0993c
aniamika <aniamika90@gmail.com>
2017-10-17 23:50:28 +0200

    0

--- sudoku.js
+++ sudoku.js
@@ -4,10 +4,84 @@ var boxesLeftTopContainer = document.querySelectorAll('.left-top-container .box'
 var boxesRightTopContainer = document.querySelectorAll('.right-top-container .box');
 var boxesLeftBottomContainer = document.querySelectorAll('.left-bottom-container .box');
 var boxesRightBottomContainer = document.querySelectorAll('.right-bottom-container .box');
+
+var blueBoxes = document.querySelectorAll('.blue');
+var lightgrayBoxes = document.querySelectorAll('.lightgray');
+var redBoxes = document.querySelectorAll('.red');
+var creamyBoxes = document.querySelectorAll('.creamy');
+var darkgrayBoxes = document.querySelectorAll('.darkgray');
+var greenBoxes = document.querySelectorAll('.green');
+var violetBoxes = document.querySelectorAll('.violet');
+var yellowBoxes = document.querySelectorAll('.yellow');
+var lightblueBoxes = document.querySelectorAll('.lightblue');
+
 var scoresLeftTop = 0;
 var scoresRightTop = 0;
 var scoresLeftBottom = 0;
 var scoresRightBottom = 0;
+
+// // hide colors untill one sudoku is done
+// function hideColors() {
+//   for(var i=0; i<blueBoxes.length; i++) {
+//     blueBoxes[i].classList.remove('blue');
+//   }
+//   for(var i=0; i<lightgrayBoxes.length; i++) {
+//     lightgrayBoxes[i].classList.remove('lightgray');
+//   }
+//   for(var i=0; i<redBoxes.length; i++) {
+//     redBoxes[i].classList.remove('red');
+//   }
+//   for(var i=0; i<creamyBoxes.length; i++) {
+//     creamyBoxes[i].classList.remove('creamy');
+//   }
+//   for(var i=0; i<darkgrayBoxes.length; i++) {
+//     darkgrayBoxes[i].classList.remove('darkgray');
+//   }
+//   for(var i=0; i<greenBoxes.length; i++) {
+//     greenBoxes[i].classList.remove('green');
+//   }
+//   for(var i=0; i<violetBoxes.length; i++) {
+//     violetBoxes[i].classList.remove('violet');
+//   }
+//   for(var i=0; i<yellowBoxes.length; i++) {
+//     yellowBoxes[i].classList.remove('yellow');
+//   }
+//   for(var i=0; i<lightblueBoxes.length; i++) {
+//     lightblueBoxes[i].classList.remove('lightblue');
+//   }
+// }
+// hideColors()
+//
+// // addcolors when sudoku is done
+// function addColors() {
+//   for(var i=0; i<blueBoxes.length; i++) {
+//     blueBoxes[i].classList.add('blue');
+//   }
+//   for(var i=0; i<lightgrayBoxes.length; i++) {
+//     lightgrayBoxes[i].classList.add('lightgray');
+//   }
+//   for(var i=0; i<redBoxes.length; i++) {
+//     redBoxes[i].classList.add('red');
+//   }
+//   for(var i=0; i<creamyBoxes.length; i++) {
+//     creamyBoxes[i].classList.add('creamy');
+//   }
+//   for(var i=0; i<darkgrayBoxes.length; i++) {
+//     darkgrayBoxes[i].classList.add('darkgray');
+//   }
+//   for(var i=0; i<greenBoxes.length; i++) {
+//     greenBoxes[i].classList.add('green');
+//   }
+//   for(var i=0; i<violetBoxes.length; i++) {
+//     violetBoxes[i].classList.add('violet');
+//   }
+//   for(var i=0; i<yellowBoxes.length; i++) {
+//     yellowBoxes[i].classList.add('yellow');
+//   }
+//   for(var i=0; i<lightblueBoxes.length; i++) {
+//     lightblueBoxes[i].classList.add('lightblue');
+//   }
+// }
 
 
 function colorize(box) {
@@ -35,25 +109,6 @@ function colorize(box) {
 
 // reset margins when finished all 4 sudoku
 function resetMargins() {
-  // if (scoresLeftTop == 3 && scoresRightTop == 3) {
-  //   document.querySelector('.left-top-container').style.marginRight = "0";
-  //   document.querySelector('.right-top-container').style.marginLeft = "0";
-  // } else if (scoresLeftTop == 3 && scoresLeftBottom == 3) {
-  //   document.querySelector('.left-top-container').style.marginBottom = "0";
-  //   document.querySelector('.left-bottom-container').style.marginTop = "0";
-  // } else if (scoresRightTop == 3 && scoresRightBottom == 3) {
-  //   document.querySelector('.right-top-container').style.marginBottom = "0";
-  //   document.querySelector('.right-bottom-container').style.marginTop = "0";
-  // } else if (scoresLeftBottom == 3 && scoresRightBottom == 3) {
-  //   document.querySelector('.left-bottom-container').style.marginRight = "0";
-  //   document.querySelector('.right-bottom-container').style.marginLeft = "0";
-  // }
-  //
-  // if (scoresLeftTop == 3 && scoresRightTop == 3 && scoresLeftBottom == 3) {
-  //   document.querySelector('.left-top-container').style.margin = "0";
-  //   document.querySelector('.right-top-container').style.margin = "0";
-  //   document.querySelector('.left-bottom-container').style.margin = "0";
-  // }
   if (scoresLeftTop == 45 && scoresRightTop == 45 && scoresLeftBottom == 45 && scoresRightBottom == 45) {
     document.querySelector('.left-top-container').style.margin = "0";
     document.querySelector('.right-top-container').style.margin = "0";
@@ -114,24 +169,25 @@ function resetBorders() {
   }
 }
 
-
-
+  // event on keypress
   for ( var i = 0; i < boxes.length; i++ ) {
     boxes[i].addEventListener("keypress", function(event) {
       var keyName = event.key;
 
       // block possibility to tap letters
-      if (keyName !== '0' && keyName != '1' && keyName != '2' && keyName != '3' && keyName != '4' && keyName != '5' && keyName != '6' && keyName != '7' && keyName != '8' && keyName != '9') {
+      if (keyName != '1' && keyName != '2' && keyName != '3' && keyName != '4' && keyName != '5' && keyName != '6' && keyName != '7' && keyName != '8' && keyName != '9') {
         event.preventDefault();
         return false;
       }
 
-
+      // if press key is the same as dataset number
       if ( keyName == this.dataset.number ) {
         this.innerText = this.dataset.number;
+        // colrize square
         colorize(this);
+        // block square editable
         this.setAttribute("contenteditable", false);
-        
+
         // count scores
         if (this.dataset.position == "leftTop") {
           scoresLeftTop = scoresLeftTop + 1;
@@ -150,7 +206,6 @@ function resetBorders() {
         resetMargins();
         resetBorders();
       } else {
-        // this.style.background = "rgb(255,255,255)";
         this.innerText = '';
       }
 
